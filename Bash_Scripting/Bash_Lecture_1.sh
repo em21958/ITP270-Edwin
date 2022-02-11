6d961db136853b2513fb42a08008a35180128f47
Edwin Morejon <em21958@email.vccs.edu>
2022-02-10 21:03:27 -0500

    class

---
+++ Bash_Scripting/Bash_Lecture_1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+#Print Hello Codecademy
+phrase="Hello Codecaedemy! using variables."
+echo $phrase
+
---
+++ Bash_Scripting/if_else.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Is it raining?"
+
+read IS_RAIN
+
+if [ $IS_RAIN == "Y" ] || [ $IS_RAIN == "y" ]
+then
+	echo "Stay at home!"
+elif [ $IS_RAIN == "N" ] || [ $IS_RAIN == "n" ]
+then
+	echo "Go out!" 
+else
+	echo "Invalid!!!!"
+fi
+
